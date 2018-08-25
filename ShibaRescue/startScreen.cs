0072aeff87a2d8ed10dd146e5eac78ccaead3cd1
Sarah Liz Dettloff <Sarah.Liz.Dettloff@gmail.com>
2018-08-25 18:08:24 -0400

    startScreen/Initial_commit

---
+++ ShibaRescue/startScreen.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShibaRescue
+{
+    public partial class startScreen : Form
+    {
+        // PixelRiver.wav Copy right goes to Trevor Lentz. Available at: https://opengameart.org/content/pixel-river
+        public startScreen()
+        {
+            InitializeComponent();
+
+        }
+
+
+        public void playStartSound()
+        {
+            var bleep = new System.Media.SoundPlayer("C:\\Users\\Sarah\\Desktop\\shibaRescueImages\\PixelRiver.wav");
+            bleep.Play();
+        }
+
+
+        public void StopStartSound()
+        {
+            var bleep = new System.Media.SoundPlayer("C:\\Users\\Sarah\\Desktop\\shibaRescueImages\\PixelRiver.wav");
+            bleep.Stop();
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StopStartSound();
+            introduction intro = new introduction();
+            intro.Show();
+            this.Hide();
+            
+
+        }
+
+        private void startScreen_Load(object sender, EventArgs e)
+        {
+            playStartSound();
+        }
+
+        private void startScreen_Close(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
